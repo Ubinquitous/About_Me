839abaa53e8d84d2ee22ba2a13d84cdc96448531
Ubinquitous <ubinquitous1@gmail.com>
2022-12-15 21:52:37 +0900

    TEST : Loading 컴포넌트 렌더링

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Loading from '../components/Loading';
+import React from 'react';
+import '../style/Home.scss';
+
+const Home = () => {
+    return (
+        <div className='home-wrap'>
+            <Loading />
+        </div>
+    );
+};
+
+export default Home;
