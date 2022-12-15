33c768f210eae78f6f10512ff242d0531c3e2a92
Ubinquitous <ubinquitous1@gmail.com>
2022-12-15 21:53:25 +0900

    ADD : Home 컴포넌트 라우터에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<div />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </Router>
   );
