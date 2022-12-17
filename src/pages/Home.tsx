35cb03f9549777a438ccb79c10935c79d392acd3
Ubinquitous <ubinquitous1@gmail.com>
2022-12-18 00:44:00 +0900

    TEST : ThreeJS Model

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,12 @@
 import Loading from '../components/Loading'
 import React from 'react'
 import '@/style/Home.scss'
+import ThreeJS from '../components/ThreeJS';
 
 const Home = () => {
+
     return (
-        <div>
-            <Loading />
-            <div className='home-wrap'>
-                <h1>test</h1>
-            </div>
-        </div>
+        <ThreeJS />
     );
 };
 
