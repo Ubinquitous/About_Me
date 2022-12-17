884b110cfc0169e8e22545a6414f6dfbd3665f4f
Ubinquitous <ubinquitous1@gmail.com>
2022-12-17 17:45:29 +0900

    SET : Home 페이지 페이징 기능 설정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,16 @@
-import Loading from '../components/Loading';
-import React from 'react';
-import '../style/Home.scss';
+import Loading from '../components/Loading'
+import React from 'react'
+import '@/style/Home.scss'
 
 const Home = () => {
     return (
-        <div className='home-wrap'>
+        <div>
             <Loading />
+            <div className='home-wrap'>
+                <h1>test</h1>
+            </div>
         </div>
     );
 };
 
-export default Home;
+export default Home
