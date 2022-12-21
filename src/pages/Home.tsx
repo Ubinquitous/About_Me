b72f35399116e08c5fd553c7406dbaaa66bdd87b
Ubinquitous <ubinquitous1@gmail.com>
2022-12-22 08:24:12 +0900

    ADD : 헤더 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Loading from '../components/Loading'
 import React from 'react'
 import '@/style/Home.scss'
 import ThreeJS from '../components/ThreeJS';
+import Header from '../components/Header';
 
 const Home = () => {
     const [isLoad, setIsLoad] = React.useState(false);
@@ -13,30 +14,33 @@ const Home = () => {
         <div className='wrap'>
             <Loading />
             {isLoad ?
-                <div className='home-wrap'>
-                    <div className='title-wrap'>
-                        <span>Park Ubin</span>
-                        <ThreeJS />
-                    </div>
-                    <div className='about-wrap'>
-                        <span className='sub-text'></span>
-                        <div className='main-about-text'>
-                            <span className='title-a gallery-font'>
-                                FRONTEND
-                            </span>
-                            <span className='title-b'>
-                                DEVELOPER
-                            </span>
-                            <span className='title-c gallery-font'>
-                                KEEP LEARN
-                            </span>
-                            <span className='title-d'>
-                                텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 <br />
-                                텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트
-                            </span>
+                <>
+                    <Header />
+                    <div className='home-wrap'>
+
+                        <div className='title-wrap'>
+                            <span>Park Ubin</span>
+                        </div>
+                        <div className='about-wrap'>
+                            <span className='sub-text'></span>
+                            <div className='main-about-text'>
+                                <span className='title-a gallery-font'>
+                                    FRONTEND
+                                </span>
+                                <span className='title-b'>
+                                    DEVELOPER
+                                </span>
+                                <span className='title-c gallery-font'>
+                                    KEEP LEARN
+                                </span>
+                                <span className='title-d'>
+                                    텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 <br />
+                                    텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트 텍스트
+                                </span>
+                            </div>
                         </div>
                     </div>
-                </div> : ''}
+                </> : ''}
         </div>
     );
 };
