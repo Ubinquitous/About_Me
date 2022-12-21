133c5599ba3bf700b5bbc6baba9a413296bbfc33
Ubinquitous <ubinquitous1@gmail.com>
2022-12-21 18:57:49 +0900

    TEST : TEST MODEL

--- src/components/ThreeJS.tsx
+++ src/components/ThreeJS.tsx
@@ -1,15 +1,23 @@
-import React from "react";
-import { Canvas } from '@react-three/fiber'
+import React, { useRef } from "react";
+import { Canvas, useFrame } from '@react-three/fiber'
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from "@react-three/drei";
 
 
 const Model = () => {
-  const gltf = useLoader(GLTFLoader, "/model/cuteMushroom/scene.gltf")
+  const cubeRef = useRef(null);
+  const gltf = useLoader(GLTFLoader, "/model/monument_valley_fan_art/scene.gltf")
+
+  useFrame((state, delta, frame) => {
+    // const mesh = gltf.scene.children[0];
+    // mesh.rotation.y = mesh.rotation.y += 0.01;
+    // mesh.rotation.x = state.clock.getElapsedTime();
+  });
+
   return (
     <>
-      <primitive position={[0, 0, 0]} object={gltf.scene} scale={0.3} />
+      <primitive ref={cubeRef} position={[0, 0, 0]} object={gltf.scene} scale={1} />
     </>
   );
 };
@@ -17,13 +25,12 @@ const Model = () => {
 
 const ThreeJS = () => {
   return (
-    <Canvas style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+    <Canvas style={{ width: '30vw', height: '60vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black' }}
       camera={{
-        position: [0, 3, 0],
-      }}
-    >
+        position: [0, 2, 0],
+      }}>
       <OrbitControls />
-      <ambientLight intensity={1.5} />
+      <ambientLight intensity={1} />
       <spotLight position={[0, 0, 0]} angle={0.3} />
       <Model />
     </Canvas>
