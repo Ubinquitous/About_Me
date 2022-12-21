df2acbe83598491b7317b86bba3f91d1f1b45763
Ubinquitous <ubinquitous1@gmail.com>
2022-12-22 08:23:01 +0900

    ADD : 헤더 추가

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/Header.scss';
+
+const Header = () => {
+    return (
+        <div className='header-wrap'>
+            <span>HEADER</span>
+        </div>
+    );
+};
+
+export default Header;
