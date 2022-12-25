861bf96746ce44354e63f65a724055de29c2b8bb
Ubinquitous <ubinquitous1@gmail.com>
2022-12-25 14:55:12 +0900

    TEST : 카드 샘플 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-
+import CardRed from './pages/card/red';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        {/* card router */}
+        <Route path='/card/red' element={<CardRed />} />
       </Routes>
     </Router>
   );
