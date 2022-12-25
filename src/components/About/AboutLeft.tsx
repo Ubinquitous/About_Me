d7563fe89cdb2a145c311dd913f615d5f46a130b
Ubinquitous <ubinquitous1@gmail.com>
2022-12-26 03:53:00 +0900

    UPDATE : 어바웃 페이지 코드 모듈화

---
+++ src/components/About/AboutLeft.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../../style/About.scss';
+
+const AboutLeft = () => {
+    return (
+        <div className='about-wrap'>
+            <span className='about-title main'
+                data-aos='fade-right'
+                data-aos-offset='100'
+                data-aos-duration='1300'
+            >TEXT<br />TEXT TEXT</span>
+            <span className='description main'
+                data-aos='fade-right'
+                data-aos-offset='120'
+                data-aos-duration='1300'
+            >Text text text text text text text text.<br />
+                text text text text text text text text text, text<br />
+                text text text text text text text text text text<br />
+                text text text text text text text text, text<br />
+                text text text text text text text text.</span>
+        </div>
+    );
+};
+
+export default AboutLeft;
---
+++ src/components/About/AboutRight.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../../style/About.scss';
+const AboutRight = () => {
+    return (
+        <div className='about-wrap item'>
+            <div className='about-span-box'>
+                <span className='about-title sub'
+                    data-aos='fade-left'
+                    data-aos-offset='100'
+                    data-aos-duration='1300'
+                >TEXT<br />TEXT TEXT</span>
+                <span className='description sub'
+                    data-aos='fade-left'
+                    data-aos-offset='120'
+                    data-aos-duration='1300'
+                >Text text text text text text text text.<br />
+                    text text text text text text text text text, text<br />
+                    text text text text text text text text.</span>
+            </div>
+        </div>
+    );
+};
+
+export default AboutRight;
