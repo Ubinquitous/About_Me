867398623e9f705d60accafd42cbe8a64f92cb09
Ubinquitous <ubinquitous1@gmail.com>
2022-12-26 03:53:32 +0900

    UPDATE : 타이틀 페이지 코드 모듈화

---
+++ src/components/Title.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/Title.scss';
+
+const Title = () => {
+    return (
+        <div className='title-wrap item'>
+            <span className='title'>Park Ubin</span>
+            <span className='sub-title'>다양한 여러가지의 기술들을 깊이있게 알아가는 것을 좋아합니다.</span>
+        </div>
+    );
+};
+
+export default Title;
